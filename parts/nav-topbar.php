b67523dd91798cce0e11c30c6fc03b8413ad7aef
Daniel Favand <daniel@favand.net>
2015-12-01 01:05:12 -0500

    inital commit

---
+++ parts/nav-topbar.php
@@ -0,0 +1,8 @@
+<div class="top-bar" id="main-menu">
+	<div class="top-bar-left">
+		<ul class="menu">
+			<li class="menu-text"><?php bloginfo('name'); ?></li>
+		</ul>
+		<?php joints_top_nav(); ?>
+	</div>
+</div>
